af4e13bcc53e622c170f24ab9bd5a16f59d43f5f
Vilson Filho <82711024+vitskrieg@users.noreply.github.com>
2022-05-11 17:28:51 -0300

    Add files via upload
    INICIO PROJEYO BACK END SISTEMA DE HOTELARIA

---
+++ SISTEMA_HOTELARIA/hotelHospede.py
@@ -0,0 +1,10 @@
+import datetime as data
+
+class hotelHospede():
+    def __init__(self,nomeHospede, cpfHospede, dataHospede):
+        self.nomeHospede = nomeHospede
+        self.cpfHospede = cpfHospede
+        self.dataHospede = dataHospede
+
+
+    
