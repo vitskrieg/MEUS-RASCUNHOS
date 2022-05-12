a6f95bfa7de33a240c561e8d3487e6e59b03233c
Vilson Filho <82711024+vitskrieg@users.noreply.github.com>
2022-05-12 18:09:10 -0300

    Update hotelHospede.py

--- SISTEMA_HOTELARIA/hotelHospede.py
+++ SISTEMA_HOTELARIA/hotelHospede.py
@@ -1,34 +1,25 @@
 from datetime import date
 from time import sleep
 
-class hotelHospede():
-    def __init__(self,nomeHospede, cpfHospede, dataHospede):
-        self.nomeHospede = nomeHospede
-        self.cpfHospede = cpfHospede
-        self.dataHospede = dataHospede
+dicionario = {}
+lista = []
 
-    def cadastroHospede(self):
-        sleep(3)
-        print(f"\n##### Nome: {self.nomeHospede}")
-        print(f"\n##### CPF:    {self.cpfHospede}")
-        print(f"\n##### Data:    {self.dataHospede.date('%d/%m/%Y')}")
-        print("\n------------------------------------------------------")
+class cadastroHospede():
+    def __init__(self):
+        pass
+    def salvarCadastro(self):
+        dicionario[int(input("Digite CPF: "))] = str(input("Digite nome: "))  
+        lista.append(dicionario)   
+        return print(lista)
+        #sleep(3)
+        #print(f"\n##### Nome: {self.nomeHospede}")
+        #print(f"\n##### CPF:  {self.cpfHospede}")
+        #print(f"\n##### Data: {self.dataHospede}")
+        #print("\n------------------------------------------------------")
         
-        botao1 = str(input("\nTudo certo com seu cadastro ? [s/n]"))
-
-        if botao1 == 's':
-            print("Salavando cadastro...")
-            sleep(3)
-            return print("HOSPEDE CADASTRADO !!!!\n")
-        else:
-            return print("Tente novamente do inicio")
-
-
 print("\n### HOTEL-TUPI-GUARANI ###\n")
 
-d1 = input("---INFORME NOME: ")
-d2 = input("\n---INFORME CPF: ")
-d3 = str(input("\n---INFORME DATA DE NASCIMENTO: "))
-
-hospede1 = hotelHospede(d1,d2,d3)
-hospede1.cadastroHospede()
+hospede = cadastroHospede()
+#usar um while, com a seguinte notação 'v == 's''
+for i in range(2): 
+    hospede.salvarCadastro()
