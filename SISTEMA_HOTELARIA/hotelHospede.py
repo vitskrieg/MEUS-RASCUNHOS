5e09ead615e7eec61cfbec3948a50ffd5f1609f5
Vilson Filho <82711024+vitskrieg@users.noreply.github.com>
2022-05-13 13:49:25 -0400

    Update hotelHospede.py

--- SISTEMA_HOTELARIA/hotelHospede.py
+++ SISTEMA_HOTELARIA/hotelHospede.py
@@ -7,19 +7,17 @@ lista = []
 class cadastroHospede():
     def __init__(self):
         pass
-    def salvarCadastro(self):
-        dicionario[int(input("Digite CPF: "))] = str(input("Digite nome: "))  
-        lista.append(dicionario)   
-        return print(lista)
-        #sleep(3)
-        #print(f"\n##### Nome: {self.nomeHospede}")
-        #print(f"\n##### CPF:  {self.cpfHospede}")
-        #print(f"\n##### Data: {self.dataHospede}")
-        #print("\n------------------------------------------------------")
-        
-print("\n### HOTEL-TUPI-GUARANI ###\n")
 
+    def salvarCadastro(self):
+        cpf  = input("\n#####  Informe seu cpf:  ")
+        nome = input("\n#### Informe seu nome")
+        dicionario[str(cpf)] = str(nome)  
+        lista.append(dicionario)   
+        return print(lista)        
+
+print("\n### HOTEL-TUPI-GUARANI ###\n")
 hospede = cadastroHospede()
-#usar um while, com a seguinte notação 'v == 's''
-for i in range(2): 
+
+r = str(input("Deseja adicionar outro hospede ? s/n  "))
+while r  == ' s ' :
     hospede.salvarCadastro()
